609c1047a8a7f02af198f4fea54130cde486a25a
Eric Carlsson <caeric@student.chalmers.se>
2019-02-16 16:23:33 +0100

    Code can now insult the user appropriatly

--- contents/content.js
+++ contents/content.js
@@ -1,5 +1,10 @@
 browser.runtime.onMessage.addListener(message => {
   if (message.command === "insult") {
-    console.log("Derp");
+    insult();
   }
 });
+
+function insult() {
+  let rand = myArray[Math.floor(Math.random() * myArray.length)];
+  alert(rand);
+}
