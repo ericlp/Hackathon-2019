82a8587135a225e94ade70bc1190e38a490d7cae
Eric Carlsson <caeric@student.chalmers.se>
2019-02-16 16:32:43 +0100

    kjlaskn

--- contents/content.js
+++ contents/content.js
@@ -5,6 +5,7 @@ browser.runtime.onMessage.addListener(message => {
 });
 
 function insult() {
+  let myArray = browser.extension.getURL("../insults.json");
   let rand = myArray[Math.floor(Math.random() * myArray.length)];
   alert(rand);
 }
