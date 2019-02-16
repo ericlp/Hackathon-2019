d6be91df2d736648734e84a673f1e6bbef99bd17
Vidar Magnusson <vidar.halmstad@hotmail.com>
2019-02-16 14:59:54 +0100

    Added support for closing tabs.

--- js/checkForBadUrls.js
+++ js/checkForBadUrls.js
@@ -1,5 +1,32 @@
-function handleUpdated(activeInfo) {
-  console.log("Tab " + activeInfo.tabId);
+function getHostname(url) {
+  if (url == null) {
+    return null;
+  }
+
+  var match = url.match(/:\/\/(www[0-9]?\.)?(.[^/:]+)/i);
+  if (
+    match != null &&
+    match.length > 2 &&
+    typeof match[2] == "string" &&
+    match[2].length > 0
+  ) {
+    return match[2];
+  }
+  return null;
 }
 
-browser.tabs.onActivated.addListener(handleUpdated);
+function handleUpdated(tabId, changeInfo, tabInfo) {
+  if (tabInfo == null) {
+    return;
+  }
+
+  var hostname = getHostname(tabInfo.url);
+  if (hostname != null) {
+    if (hostname == "chalmers.it") {
+      console.log("SHOULD CLOSE YE FUCK!" + tabInfo.tabId);
+      browser.tabs.remove(tabInfo.id);
+    }
+  }
+}
+
+browser.tabs.onUpdated.addListener(handleUpdated);
